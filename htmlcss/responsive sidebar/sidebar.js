b34365c215d982b33b0b96cdc92596dc46ccc907
Keyur Bhuta <bhutakeyur0208@gmail.com>
2025-06-04 12:18:27 +0530

    html/css assignments

---
+++ htmlcss/responsive sidebar/sidebar.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body");
+const sidebar = document.querySelector(".sidebar");
+const toggle = document.querySelector(".arrow-left");
+const modeSwitch = document.querySelector(".toggle-switch");
+const modeText = document.querySelector(".mode-text");
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light Mode";
+    } else {
+        modeText.innerText = "Dark Mode";
+    }
+});
